5dc1209185e8a037fa5cd001ea011eab0e6a40f2
Nimesh Kumar <nimesh.kumar031@gmail.com>
2019-05-29 13:00:57 +0530

    Add files via upload

---
+++ device-iot-app.py
@@ -0,0 +1,41 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import csv
+
+appname=[]
+appdesc=[]
+applink=[]
+appcategory='Devices and IOT';
+page=1
+while(page<3):
+	url = "https://snapcraft.io/search?category=devices-and-iot&page="+str(page)
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text,"html.parser")
+
+	for i in soup.findAll('a',attrs={'class':'p-media-object'},href=True):
+		if i.text: 
+			url1 ="https://snapcraft.io"+i['href']
+			response = requests.get(url1)
+			soup = BeautifulSoup(response.text,"html.parser")
+			for i in soup.findAll('div',attrs={'class':'p-snap-heading'}):
+				appname.append(i.find('h1').text)
+			for j in soup.findAll('h4',attrs={'data-live':'summary'}):
+				appdesc.append(j.text)
+			for k in soup.findAll('div',attrs={'class':'p-code-snippet'}):
+				a = k.find('input')['value']
+				myString = re.sub(r"[\n\t]*", "",a)
+				applink.append(myString)
+
+	page=page+1			
+
+with open('devices-and-iot.csv', 'a') as csvFile:
+    writer = csv.writer(csvFile)
+    for i in range(len(appname)):
+   		writer.writerow([i+1,appname[i],appdesc[i],applink[i],appcategory])
+
+		
+
+
+	
---
+++ server-cloud-app.py
@@ -0,0 +1,41 @@
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+import re
+import csv
+
+appname=[]
+appdesc=[]
+applink=[]
+appcategory='Server and cloud';
+page=1
+while(page<4):
+	url = "https://snapcraft.io/search?category=server-and-cloud&page="+str(page)
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text,"html.parser")
+
+	for i in soup.findAll('a',attrs={'class':'p-media-object'},href=True):
+		if i.text: 
+			url1 ="https://snapcraft.io"+i['href']
+			response = requests.get(url1)
+			soup = BeautifulSoup(response.text,"html.parser")
+			for i in soup.findAll('div',attrs={'class':'p-snap-heading'}):
+				appname.append(i.find('h1').text)
+			for j in soup.findAll('h4',attrs={'data-live':'summary'}):
+				appdesc.append(j.text)
+			for k in soup.findAll('div',attrs={'class':'p-code-snippet'}):
+				a = k.find('input')['value']
+				myString = re.sub(r"[\n\t]*", "",a)
+				applink.append(myString)
+
+	page=page+1			
+
+with open('server-and-cloud.csv', 'a') as csvFile:
+    writer = csv.writer(csvFile)
+    for i in range(len(appname)):
+   		writer.writerow([i+1,appname[i],appdesc[i],applink[i],appcategory])
+
+		
+
+
+	
